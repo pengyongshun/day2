7d2ef6ee6978596a8def8880c44aa4648be7c8b1
pengyongshun <740604387@qq.com>
2016-06-28 19:01:31 +0800

    第一个Activity

---
+++ app/src/main/java/day1/app/pys/com/pengyongshun/MainActivity.java
@@ -0,0 +1,13 @@
+package day1.app.pys.com.pengyongshun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
